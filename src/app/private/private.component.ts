d87bbe43b91e0f397544e6c120a28c3fc80a1a3e
advatixNaved <162119767+advatixNaved@users.noreply.github.com>
2024-12-30 15:29:51 +0530

    some architechture changes

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-private',
-  standalone: false,
   templateUrl: './private.component.html',
   styleUrl: './private.component.scss'
 })
